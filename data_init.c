eab5d83c9ddd2a7f0508317e77d30cfef0e8bd4a
Anoir Baya <anbaya@c4r10p9.1337.ma>
2025-01-23 15:56:38 +0100

    done

--- data_init.c
+++ data_init.c
@@ -34,10 +34,6 @@ int	data_init(t_data *data, char *map)
 	data->mlx = mlx_init();
 	data->win = mlx_new_window(data->mlx, SIZE * ft_strlen(data->map_len),
 			data->map_lines * SIZE, "so_long");
-	data->move = 0;
 	data->str = ft_itoa(data->move);
-	data->frame = 0;
-	data->x = 0;
-	data->y = 0;
 	return (data->act = 0, 0);
 }
--- enemy.c
+++ enemy.c
@@ -17,7 +17,6 @@ void	enemy(t_data *data)
 	data->move++;
 	data->str = ft_itoa(data->move);
 	mlx_string_put(data->mlx, data->win, 200, 200, 0xFF0000, data->str);
-	free(data->str);
 	ft_putnbr(data->move);
 	write(1, "\n", 1);
 	perror("you dead !");
